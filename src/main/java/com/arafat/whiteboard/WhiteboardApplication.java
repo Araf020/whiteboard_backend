e2d2bd8590b312d70bf164b2ee9f96e22359756b
Araf020 <Araf020@users.noreply.github.com>
2023-01-26 17:06:04 +0600

    updated

--- src/main/java/com/arafat/whiteboard/WhiteboardApplication.java
+++ src/main/java/com/arafat/whiteboard/WhiteboardApplication.java
@@ -2,11 +2,14 @@ package com.arafat.whiteboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class WhiteboardApplication {
-
+    
+    
     public static void main(String[] args) {
+        
         SpringApplication.run(WhiteboardApplication.class, args);
     }
 
